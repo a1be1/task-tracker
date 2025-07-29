87e2599a5fdbdb709073de03cb06fca33ed15f0c
a.belov <aleksandr.belov@semrush.com>
2025-07-29 11:12:06 +0200

    initial commit

---
+++ src/test/java/com/family_tasks/tracker/TrackerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.family_tasks.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
