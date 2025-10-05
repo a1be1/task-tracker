f410a05af6378cff1438c5baee19553694c121ff
a.belov <aleksandr.belov@semrush.com>
2025-10-05 15:14:30 +0200

    KAN-24.Covered the tasks APIs with documentation

--- src/main/java/com/family_tasks/tracker/task/model/dto/TaskApiResponse.java
+++ src/main/java/com/family_tasks/tracker/task/model/dto/TaskApiResponse.java
@@ -2,6 +2,7 @@ package com.family_tasks.tracker.task.model.dto;
 
 import com.family_tasks.tracker.task.model.enums.TaskPriority;
 import com.family_tasks.tracker.task.model.enums.TaskStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -17,15 +18,26 @@ import java.util.Set;
 @EqualsAndHashCode
 public class TaskApiResponse {
 
+    @Schema(description = "The task ID.")
     private final String taskId;
+    @Schema(description = "The task name.")
     private final String name;
+    @Schema(description = "The task status.", implementation = TaskStatus.class)
     private final TaskStatus status;
+    @Schema(description = "The task description.")
     private final String description;
+    @Schema(description = "The task priority. Affects ordering in the journal.", implementation = TaskPriority.class)
     private final TaskPriority priority;
+    @Schema(description = "The user ID of the task creator.")
     private final Integer reporterId;
+    @Schema(description = "The list of user IDs of the executors of the task.")
     private final Set<Integer> executorIds;
+    @Schema(description = "The flag indicates whether the task is public or visible only for the reporter and executors.")
     private final boolean confidential;
+    @Schema(description = "The task deadline indicates when the task is expected to be completed.")
     private final LocalDate deadline;
+    @Schema(description = "The datetime of the task creation.")
     private final LocalDateTime createdAt;
+    @Schema(description = "The datetime of the last task update.")
     private final LocalDateTime updatedAt;
 }
--- src/main/java/com/family_tasks/tracker/task/model/dto/TaskCreateApiRequest.java
+++ src/main/java/com/family_tasks/tracker/task/model/dto/TaskCreateApiRequest.java
@@ -1,6 +1,8 @@
 package com.family_tasks.tracker.task.model.dto;
 
 import com.family_tasks.tracker.common.validation.annotation.ValidTaskPriority;
+import com.family_tasks.tracker.task.model.enums.TaskPriority;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -14,29 +16,52 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-import static com.family_tasks.tracker.common.validation.ValidationConstants.*;
-import static com.family_tasks.tracker.common.validation.ValidationMessage.*;
+import static com.family_tasks.tracker.common.validation.ValidationConstants.TASK_DESCRIPTION_MAX_LENGTH;
+import static com.family_tasks.tracker.common.validation.ValidationConstants.TASK_DESCRIPTION_MIN_LENGTH;
+import static com.family_tasks.tracker.common.validation.ValidationConstants.TASK_NAME_MAX_LENGTH;
+import static com.family_tasks.tracker.common.validation.ValidationMessage.TASK_CONFIDENTIAL_STATUS_NOT_SPECIFIED;
+import static com.family_tasks.tracker.common.validation.ValidationMessage.TASK_DEADLINE_DATE_NOT_FUTURE;
+import static com.family_tasks.tracker.common.validation.ValidationMessage.TASK_DESCRIPTION_TOO_LONG;
+import static com.family_tasks.tracker.common.validation.ValidationMessage.TASK_DESCRIPTION_TOO_SHORT;
+import static com.family_tasks.tracker.common.validation.ValidationMessage.TASK_NAME_NOT_SPECIFIED;
+import static com.family_tasks.tracker.common.validation.ValidationMessage.TASK_NAME_TOO_LONG;
+import static com.family_tasks.tracker.common.validation.ValidationMessage.TASK_PRIORITY_NULL;
+import static com.family_tasks.tracker.common.validation.ValidationMessage.TASK_REPORTER_NULL;
 
 @Getter
 @Builder
 @ToString
 @EqualsAndHashCode
 public class TaskCreateApiRequest {
+
+    @Schema(description = "The task name.")
     @Size(max = TASK_NAME_MAX_LENGTH, message = TASK_NAME_TOO_LONG)
     @NotEmpty(message = TASK_NAME_NOT_SPECIFIED)
     private final String name;
+
+    @Schema(description = "The task description.")
     @Size(max = TASK_DESCRIPTION_MAX_LENGTH, message = TASK_DESCRIPTION_TOO_LONG)
     @Size(min = TASK_DESCRIPTION_MIN_LENGTH, message = TASK_DESCRIPTION_TOO_SHORT)
     private final String description;
+
+    @Schema(description = "The task priority. Affects ordering in the journal.", implementation = TaskPriority.class)
     @ValidTaskPriority
     @NotEmpty(message = TASK_PRIORITY_NULL)
     private final String priority;
+
+    @Schema(description = "The user ID of the task creator.")
     @NotNull(message = TASK_REPORTER_NULL)
     private final Integer reporterId;
+
+    @Schema(description = "The list of user IDs of the executors of the task.")
     @Builder.Default
     private final Set<Integer> executorIds = new HashSet<>();
+
+    @Schema(description = "The flag indicates whether the task is public or visible only for the reporter and executors.")
     @NotNull(message = TASK_CONFIDENTIAL_STATUS_NOT_SPECIFIED)
     private final Boolean confidential;
+
+    @Schema(description = "The task deadline indicates when the task is expected to be completed.")
     @Future(message = TASK_DEADLINE_DATE_NOT_FUTURE)
     private final LocalDate deadline;
 }
