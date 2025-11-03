c05cf6eee3a1458e3facc8393011c23203a87525
KakEnot <k.a.isaeva@icloud.com>
2025-11-03 12:47:26 +0100

    Add dokumentation to controllers

--- src/main/java/com/family_tasks/tracker/group/application/GroupController.java
+++ src/main/java/com/family_tasks/tracker/group/application/GroupController.java
@@ -3,6 +3,8 @@ package com.family_tasks.tracker.group.application;
 import com.family_tasks.tracker.group.core.GroupCreateService;
 import com.family_tasks.tracker.group.model.dto.GroupApiResponse;
 import com.family_tasks.tracker.group.model.dto.GroupCreateApiRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,12 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Group management", description = "Operations for managing groups")
 public class GroupController {
 
     public static final String GROUP_URL = "/v1/groups";
 
     private final GroupCreateService groupCreateService;
 
+    @Operation(
+            summary = "Create a new group",
+            description = """
+                    Creates a new group in the system.
+                    
+                    **Rules and constraints:**
+                    - A user cannot own more than one group.
+                    - A user can be a member of only one group.
+                    - A user without a group cannot create or edit tasks.
+                    """
+    )
     @PostMapping(GROUP_URL)
     public GroupApiResponse createGroup(@Valid @RequestBody GroupCreateApiRequest apiRequest) {
         return groupCreateService.createGroup(apiRequest);
--- src/main/java/com/family_tasks/tracker/task/application/TaskController.java
+++ src/main/java/com/family_tasks/tracker/task/application/TaskController.java
@@ -9,6 +9,8 @@ import com.family_tasks.tracker.task.model.dto.TaskCreateApiRequest;
 import com.family_tasks.tracker.task.model.dto.TaskFilterRequest;
 import com.family_tasks.tracker.task.model.dto.TaskUpdateApiRequest;
 import com.family_tasks.tracker.task.model.enums.TaskFilter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +25,7 @@ import static com.family_tasks.tracker.common.validation.ValidationMessage.USER_
 @Validated
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Task management", description = "Operations for managing tasks")
 public class TaskController {
 
     public static final String TASK_URL = "/v1/tasks";
@@ -31,16 +34,45 @@ public class TaskController {
     private final TaskUpdateService taskUpdateService;
     private final TaskGetService taskGetService;
 
+    @Operation(
+            summary = "Create a new task",
+            description = """
+                    This endpoint creates a new task in the system.
+                    
+                    **Rules and constraints:**
+                    - Users without a group cannot create tasks.
+                    - Executors and the task reporter must belong to the same group.
+                    """
+    )
     @PostMapping(TASK_URL)
     public TaskApiResponse createTask(@Valid @RequestBody TaskCreateApiRequest apiRequest) {
         return taskCreateService.createTask(apiRequest);
     }
 
+    @Operation(
+            summary = "Update an existing task",
+            description = """
+                    This endpoint updates an existing task in the system.
+                    
+                    **Rules and constraints:**
+                    - Users without a group cannot update tasks.
+                    - Executors and the task reporter must belong to the same group.
+                    """
+    )
     @PutMapping(TASK_URL + "/{taskId}")
     public TaskApiResponse updateTask(@PathVariable String taskId, @Valid @RequestBody TaskUpdateApiRequest apiRequest) {
         return taskUpdateService.updateTask(taskId, apiRequest);
     }
 
+    @Operation(
+            summary = "Get an existing task",
+            description = """
+                    This endpoint retrieves a task by its unique ID.
+                    
+                    **Rules and constraints:**
+                    - If 'confidential' is 'true', only the task reporter or assigned executors can view the task.
+                    """
+    )
     @GetMapping(TASK_URL + "/{taskId}")
     public TaskApiResponse getTask(@PathVariable String taskId,
                                    @NotNull(message = USER_NOT_SPECIFIED)
@@ -48,6 +80,21 @@ public class TaskController {
                                    Integer userId) {
         return taskGetService.getTask(taskId, userId);
     }
+
+    @Operation(
+            summary = "Get all tasks from group",
+            description = """
+                    This endpoint returns all tasks from a user's group, filtered by TaskFilter enum.
+                    
+                    **Filter values:**\n
+                    'ALL_AVAILABLE': Returns all tasks visible to the current user, excluding confidential ones.\n
+                    'ALL_CLOSED': Returns all tasks from the user's group that are marked as cancelled.\n
+                    'IS_REPORTER_ACTIVE_TASK': Active (open) tasks **created by** the current user.\n
+                    'IS_EXECUTOR_ACTIVE_TASK': Active (open) tasks **assigned to** the current user.\n
+                    'IS_REPORTER_COMPLETED_TASK': Completed tasks **created by** the current user.\n
+                    'IS_EXECUTOR_COMPLETED_TASK': Completed tasks **assigned to** the current user.
+                    """
+    )
 
     @GetMapping(TASK_URL)
     public List<TaskApiResponse> getTasks(@NotNull(message = USER_NOT_SPECIFIED)
