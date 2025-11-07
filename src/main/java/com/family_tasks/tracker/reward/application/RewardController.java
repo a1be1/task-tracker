a42592187d906714d6b111676387be0d2db0ddf0
KakEnot <k.a.isaeva@icloud.com>
2025-11-07 13:48:29 +0100

    adding Dokumentation

--- src/main/java/com/family_tasks/tracker/reward/application/RewardController.java
+++ src/main/java/com/family_tasks/tracker/reward/application/RewardController.java
@@ -2,6 +2,8 @@ package com.family_tasks.tracker.reward.application;
 
 import com.family_tasks.tracker.reward.core.RewardGetService;
 import com.family_tasks.tracker.reward.model.dto.RewardApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +16,20 @@ import static com.family_tasks.tracker.common.validation.ValidationMessage.USER_
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Reward management", description = "Operations for managing rewards")
 public class RewardController {
 
     public static final String REWARD_URL = "/v1/rewards";
 
     private final RewardGetService rewardGetService;
 
+    @Operation(
+            summary = "Get all rewards by user ID",
+            description = """
+                    This endpoints returns all user's rewards. If the user
+                    doesn't have any rewards, it returns an empty list.
+                    """
+    )
     @GetMapping(REWARD_URL)
     public List<RewardApiResponse> getRewards(@NotNull(message = USER_NOT_SPECIFIED)
                                               @RequestParam(name = "userId", required = false)
